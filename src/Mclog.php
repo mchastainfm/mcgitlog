9bdf4957188c3074b4124b356d50dc234c5ea6b5
Mike Chastain <mchastain@fuelmedical.com>
2017-01-20 16:35:54 -0800

    changes location of log

--- src/Mclog.php
+++ src/Mclog.php
@@ -15,7 +15,7 @@ class mclog
                 $toLog = (array) $logVar;
             }
 
-            file_put_contents('/vagrant/mclog/log.json', "{\n" . $logMessage . ":  " .  json_encode($toLog, JSON_PRETTY_PRINT)  . "\n\n}" . $end, FILE_APPEND);
+            file_put_contents('./log.json', "{\n" . $logMessage . ":  " .  json_encode($toLog, JSON_PRETTY_PRINT)  . "\n\n}" . $end, FILE_APPEND);
         }
     public static function startLog($logname = '')
         {
@@ -23,7 +23,7 @@ class mclog
 
         $t = time();
         exec("cp /vagrant/mclog/log.json /vagrant/mclog/logb.json");
-            file_put_contents('/vagrant/mclog/log.json', "{\"" . $logname . "\": \"" .  date("Y-m-d h:i:sa",$t) ."\"\n\n");
+            file_put_contents(./log.json', "{\"" . $logname . "\": \"" .  date("Y-m-d h:i:sa",$t) ."\"\n\n");
         }
 
 }
